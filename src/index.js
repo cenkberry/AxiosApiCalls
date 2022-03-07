2ad6b0b8c95dfef0aa84d934ae7bb4b19eb2aae5
cenkberry <cenkberi@gmail.com>
2022-03-07 18:27:47 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ const btnTrn = document.querySelector('.btnTrn');
 const btnErr = document.querySelector('.btnErr');
 const btnCan = document.querySelector('.btnCan');
 
+btnGet.addEventListener("click" , getPosts());
+btnPos.addEventListener("click" , postIt());
+btnPut.addEventListener("click" , putIt());
+btnDel.addEventListener("click" , deleteIt());
+btnSim.addEventListener("click" , simReq());
+btnCus.addEventListener("click" , customHeaders());
