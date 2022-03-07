afd9c43fabcad0f7509a7f664a1334546c430d14
cenkberry <cenkberi@gmail.com>
2022-03-07 18:32:34 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -8,9 +8,35 @@ const btnTrn = document.querySelector('.btnTrn');
 const btnErr = document.querySelector('.btnErr');
 const btnCan = document.querySelector('.btnCan');
 
-btnGet.addEventListener("click" , getPosts());
-btnPos.addEventListener("click" , postIt());
-btnPut.addEventListener("click" , putIt());
-btnDel.addEventListener("click" , deleteIt());
-btnSim.addEventListener("click" , simReq());
-btnCus.addEventListener("click" , customHeaders());
+btnGet.addEventListener("click" , getPosts);
+btnPos.addEventListener("click" , postIt);
+btnPut.addEventListener("click" , putIt);
+btnDel.addEventListener("click" , deleteIt);
+btnSim.addEventListener("click" , simReq);
+btnCus.addEventListener("click" , customHeaders);
+
+
+
+function getPosts(){
+  console.log("getPosts")
+}
+
+function postIt(){
+  console.log("Post it")
+}
+
+function putIt(){
+  console.log("Put it")
+}
+
+function deleteIt(){
+  console.log("delete it")
+}
+
+function simReq(){
+  console.log("sim request")
+}
+
+function customHeaders(){
+  console.log("sim request")
+}
